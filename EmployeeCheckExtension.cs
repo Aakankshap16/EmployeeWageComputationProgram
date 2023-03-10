b40736912d8bf544a474db22a90407e835d3a1c1
Aakankshap16 <aakankshap971@gmail.com>
2023-03-10 14:49:14 +0530

    full with half time

---
+++ EmployeeCheckExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation_Problem
+{
+    internal class EmployeeCheckExtension
+    {
+        public static int EmployeeAttendence(int empSalPerhr)
+        {
+            int is_Full_Time = 1;
+            int is_Part_Time = 2;
+
+            int empWage = 0;
+            int empHrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(3);
+            if (empCheck == is_Full_Time)
+            {
+                empHrs = 8;
+            }
+            else if (empCheck == is_Part_Time)
+            {
+                empHrs = 4;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empSalPerhr;
+            return empWage;
+
+        }
+    }
+}
