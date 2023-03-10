be98d804ba3a18023d868053d50e9118a8e2ef37
Aakankshap16 <aakankshap971@gmail.com>
2023-03-10 14:42:44 +0530

    fulltime only

---
+++ EmployeeCheck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Wage_Computation_Problem
+{
+    internal class EmployeeCheck
+    {
+        public static void EmployeeAttendence()
+        {
+            int is_Full_Time = 1;
+            int empSal = 20;
+            int empWage = 0;
+            int empHrs = 0;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+            if (empCheck == is_Full_Time)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empSal;
+            Console.WriteLine("wages of employee is :" + empWage);
+        }
+    }
+}
