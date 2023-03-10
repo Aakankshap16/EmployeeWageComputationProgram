0a7d4e02c303225b8dcf5671c75a7b3f2809116f
Aakankshap16 <aakankshap971@gmail.com>
2023-03-10 14:34:25 +0530

    program file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Employee_Wage_Computation_Problem 
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the employee wage computation problem!");
+            
+
+        }
+    }
+
+}
