9bbe2bdf14b8501d418483cf7ecf022feeb947b3
Aakankshap16 <aakankshap971@gmail.com>
2023-03-10 14:54:00 +0530

    with uc3

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@
             Console.WriteLine("Welcome to the employee wage computation problem!");
            // EmployeeCheck.EmployeeAttendence();
             int empSalPerhr = 20;
-            int empWageValue = EmployeeCheckExtension.EmployeeAttendence(empSalPerhr);
+            //int empWageValue = EmployeeCheckExtension.EmployeeAttendence(empSalPerhr);
+            int empWageValue = usingSwitchCase.EmployeeAttendence(empSalPerhr);
             Console.WriteLine("wages of employee is :" + empWageValue);
         }
     }
